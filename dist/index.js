b5fb7856f53c26ee77b5e420bfcf74ae9d9b5a93
Alejandro <apramos89@gmail.com>
2019-05-03 11:32:42 +0200

    Error upload

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var Queue_1 = __importDefault(require("./Queue"));
-var PriorityQueue_1 = __importDefault(require("./PriorityQueue"));
-exports.default = { Queue: Queue_1.default, PriorityQueue: PriorityQueue_1.default };
-//# sourceMappingURL=index.js.map
